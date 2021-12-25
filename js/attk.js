6c7db2b5d22ef01a93d053717af8dc347d09b146
crixxcroxx <cristajesawa@gmail.com>
2021-12-25 11:11:36 +0800

    Create helper function for during battle event
    Remove repetitive code

--- js/attk.js
+++ js/attk.js
@@ -30,13 +30,13 @@ export function attk(caster, target, types) {
 
   //total damage formula based on bulbapedia.net
   total_dmg = Math.round((1000 * /**/(((((2 * pokemon.level) / 5) + 2 ) * move.power * (attack / defense) / 50) + 2)/**/) / 1000)
-  
+
   //checks the damage type for effectiveness of the attack
   //before calculating the final damage
   let dmg_type = ``
   target.pokemon.type.map(t => {
     let mov = types[move.type]
-    
+
     for(let key in mov) {
       let idx = mov[key].findIndex(i => i == t)
       if(idx != -1) { dmg_type = key; break; }
@@ -46,17 +46,13 @@ export function attk(caster, target, types) {
   //attack is super effective
   //damage is doubled
   if(dmg_type == `double_damage_to`) {
+    const message = `It's super effective!`
     target.hp.current_hp -= (total_dmg * 2)
 
-    if(target.hp.current_hp <= 0) {
-      utils.$(`${hp_bar}`).style.width = 0
-    } else {
-      utils.$(`${hp_bar}`).style.width = `${100 / (target.hp.max_hp / target.hp.current_hp)}%`
-    }
-
-    utils.delay(1000).then(() => {
-      utils.$(`#message`).innerText = `It's super effective!`
-    })
+    battleEvent({
+      current_hp: target.hp.current_hp,
+      max_hp: target.hp.max_hp
+    }, hp_bar, message)
 
     return (total_dmg * 2)
   }
@@ -64,34 +60,26 @@ export function attk(caster, target, types) {
   //attack is not very effective
   //damage is halved
   else if(dmg_type == `half_damage_to`) {
+    const message = `It's not very effective.`
     target.hp.current_hp -= (total_dmg / 2)
 
-    if(target.hp.current_hp <= 0) {
-      utils.$(`${hp_bar}`).style.width = 0
-    } else {
-      utils.$(`${hp_bar}`).style.width = `${100 / (target.hp.max_hp / target.hp.current_hp)}%`
-    }
-
-    utils.delay(1000).then(() => {
-      utils.$(`#message`).innerText = `It's not very effective.`
-    })
+    battleEvent({
+      current_hp: target.hp.current_hp,
+      max_hp: target.hp.max_hp
+    }, hp_bar, message)
 
     return (total_dmg / 2)
   }
-  
+
   //attack has no damage
   else if (dmg_type == `no_damage_to`) {
+    const message = `Dealt no damage.`
     target.hp.current_hp -= 0
 
-    if(target.hp.current_hp <= 0) {
-      utils.$(`${hp_bar}`).style.width = 0
-    } else {
-      utils.$(`${hp_bar}`).style.width = `${100 / (target.hp.max_hp / target.hp.current_hp)}%`
-    }
-
-    utils.delay(1000).then(() => {
-      utils.$(`#message`).innerText = `Dealt no damage.`
-    })
+    battleEvent({
+      current_hp: target.hp.current_hp,
+      max_hp: target.hp.max_hp
+    }, hp_bar, message)
 
     return 0
   }
@@ -100,13 +88,29 @@ export function attk(caster, target, types) {
   else {
     target.hp.current_hp -= total_dmg
 
-    if(target.hp.current_hp <= 0) {
-      utils.$(`${hp_bar}`).style.width = 0
-    } else {
-      utils.$(`${hp_bar}`).style.width = `${100 / (target.hp.max_hp / target.hp.current_hp)}%`
-    }
+    battleEvent({
+      current_hp: target.hp.current_hp,
+      max_hp: target.hp.max_hp
+    }, hp_bar, false)
 
     return total_dmg
   }
-  
+
+}
+
+//helper function
+const battleEvent = (hp, hp_bar, message) => {
+  const {current_hp, max_hp} = hp
+
+  if(current_hp <= 0) {
+    utils.$(`${hp_bar}`).style.width = 0
+  } else {
+    utils.$(`${hp_bar}`).style.width = `${100 / (max_hp / current_hp)}%`
+  }
+
+  if(message) {
+    utils.delay(1000).then(() => {
+      utils.$(`#message`).innerText = message
+    })
+  }
 }
